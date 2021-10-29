6a4da2668f565e3acd9304decac3ce22a884832b
Norah Hannah Schat <nschat@student.codam.nl>
2021-10-29 13:06:59 +0200

    feat: add ex02 pointers/references

---
+++ ex02/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nschat <nschat@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/10/29 13:02:01 by nschat        #+#    #+#                 */
+/*   Updated: 2021/10/29 13:05:47 by nschat        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string brain = "HI THIS IS BRAIN";
+	std::string *stringPTR = &brain;
+	std::string &stringREF = brain;
+
+	std::cout << &brain << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+}
