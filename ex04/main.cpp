1fc8b8085f80fd2995746c8f83a1a893bfdd8827
Norah Hannah Schat <nschat@student.codam.nl>
2021-11-01 15:37:36 +0100

    feat: don't use string::replace

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nschat <nschat@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/01 13:22:51 by nschat        #+#    #+#                 */
-/*   Updated: 2021/11/01 14:19:52 by nschat        ########   odam.nl         */
+/*   Updated: 2021/11/01 15:37:11 by nschat        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,20 @@ int	main(int ac, char **av) {
 	}
 	std::ofstream outfile(filename.append(".replace"));
 	if (outfile.is_open()) {
+		size_t i = 0;
 		size_t pos = 0;
+		size_t len = 0;
 		while (pos != std::string::npos) {
 			pos = content.find(find, pos);
-			if (pos != std::string::npos)
-				content.replace(pos, find.length(), replace);
+			len = pos - i;
+			if (pos != std::string::npos) {
+				outfile << content.substr(i, len);
+				outfile << replace;
+				pos += find.length();
+				i = pos;
+			}
 		}
-		outfile << content;
+		outfile << std::endl;
 	} else {
 		std::cout << "Error: failed to open output file" << std::endl;
 		return (4);
