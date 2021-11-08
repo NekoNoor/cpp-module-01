750a7dd7e7f20927a63003b1d34001e54ef0cf74
Norah Hannah Schat <nschat@student.codam.nl>
2021-11-08 16:10:38 +0100

    feat: add karenFilter

---
+++ ex06/main.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nschat <nschat@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/11/02 10:40:31 by nschat        #+#    #+#                 */
+/*   Updated: 2021/11/08 16:09:51 by nschat        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Karen.hpp"
+
+
+static const std::string g_log_levels[] = {
+	"DEBUG",
+	"INFO",
+	"WARNING",
+	"ERROR"
+};
+
+static int log_level_int(std::string input) {
+	for (int i = 0; i < 4; i++)
+	{
+		if (input.compare(g_log_levels[i]) == 0)
+			return (i);
+	}
+	return (-1);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac != 2) {
+		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+		return (1);
+	}
+	Karen annoying = Karen();
+	std::string str(av[1]);
+	switch (log_level_int(av[1])) {
+		case (-1):
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+			break;
+		case (0):
+			annoying.complain(g_log_levels[0]);
+		case (1):
+			annoying.complain(g_log_levels[1]);
+		case (2):
+			annoying.complain(g_log_levels[2]);
+		case (3):
+			annoying.complain(g_log_levels[3]);
+	}
+	return (0);
+}
